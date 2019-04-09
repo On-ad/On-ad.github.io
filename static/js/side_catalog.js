8e1cd5235cac345f124b45b15beb13004c6bbf04
hwasurr <hwasurr@MacBookProui-MacBookPro.local>
2019-04-10 00:40:55 +0900

    픽스 위치 수정

--- static/js/side_catalog.js
+++ static/js/side_catalog.js
@@ -17,7 +17,7 @@ jQuery(document).ready(function($) {
 
               //adjust the appearance of side-catalog
               $catalog.show()
-              if (currentTop > (bannerHeight - 10)) {
+              if (currentTop > (bannerHeight + 41)) {
                   $catalog.addClass('fixed')
               } else {
                   $catalog.removeClass('fixed')
